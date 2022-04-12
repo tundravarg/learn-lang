951713b2e11a8c741c51990ead2e89a0a3292a00
Sergey Tumanov <tumanser@gmail.com>
2022-04-12 10:56:56 +0300

    Init Kotlin project (doesn't run)

---
+++ kotlin/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WD=target
+JAR=learn-kotlin-1.0-SNAPSHOT.jar
+MC=tuman.learn.kotlin.AppKt
+
+cd "$WD" && java -cp "$JAR" $MC $*
---
+++ kotlin/src/main/kotlin/tuman/learn/kotlin/App.kt
@@ -0,0 +1,6 @@
+package tuman.learn.kotlin
+
+
+fun main(args: Array<String>) {
+    System.out.println("Learn Kotlin");
+}
