e2c33020821f0dcf3f172bf1acf625072ac4794c
Sergei Tumanov <tumanser@gmail.com>
2022-05-03 09:00:36 +0300

    Location model

---
+++ java/src/main/java/tuman/learn/java/model/Location.java
@@ -0,0 +1,98 @@
+package tuman.learn.java.model;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Location {
+
+    public enum Type {
+        OTHER,
+        BUILDING,
+        FLOOR,
+        SECTION,
+        ROOM,
+        ZONE,
+        CABINET,
+        CONTAINER
+    }
+
+
+    private static int nextId = 1;
+
+    public static int nextId() {
+        return nextId++;
+    }
+
+    public static void resetId(int value) {
+        nextId = value;
+    }
+
+    public static void resetId() {
+        resetId(1);
+    }
+
+
+    private Integer id;
+    private String name;
+    private Type type;
+    private Location parent;
+    private Set<Location> children = new HashSet<>();
+    private double area;
+    private double volume;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Location getParent() {
+        return parent;
+    }
+
+    public void setParent(Location parent) {
+        this.parent = parent;
+    }
+
+    public Set<Location> getChildren() {
+        return children;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+}
