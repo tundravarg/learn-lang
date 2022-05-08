e50a9d3b46e445d2f375c66d5ed5946857c34470
Sergei Tumanov <tumanser@gmail.com>
2022-05-08 16:11:46 +0300

    Remove redundant tests

--- java/src/main/java/tuman/learn/java/LearnGenerics.java
+++ java/src/main/java/tuman/learn/java/LearnGenerics.java
@@ -10,171 +10,11 @@ public class LearnGenerics {
     }
 
     public void superAndExtends() {
-        Holder<Object> holderOfObject = new Holder<>();
-        Holder<A1> holderOfA1 = new Holder<>();
-        Holder<A2> holderOfA2 = new Holder<>();
-        Holder<A3> holderOfA3 = new Holder<>();
-        Holder<A4> holderOfA4 = new Holder<>();
-        Holder<A5> holderOfA5 = new Holder<>();
-        Holder<? super A3> holderOfA3Super = new Holder<>();
-        Holder<? extends A3> holderOfA3Extent = new Holder<>();
-
-        Object o = new Object();
         A1 a1 = new A1();
         A2 a2 = new A2();
         A3 a3 = new A3();
         A4 a4 = new A4();
         A5 a5 = new A5();
-
-        // Check
-
-        a1.a1();
-        a2.a2();
-        a3.a3();
-        a4.a4();
-        a5.a5();
-
-        // Field
-
-//        holderOfA3.object = o;  // Illegal
-//        holderOfA3.object = a1; // Illegal
-//        holderOfA3.object = a2; // Illegal
-        holderOfA3.object = a3;
-        holderOfA3.object = a4;
-        holderOfA3.object = a5;
-
-//        holderOfA3Super.object = o;  // Illegal
-//        holderOfA3Super.object = a1; // Illegal
-//        holderOfA3Super.object = a2; // Illegal
-        holderOfA3Super.object = a3;
-        holderOfA3Super.object = a4;
-        holderOfA3Super.object = a5;
-
-//        holderOfA3Extent.object = o;  // Illegal
-//        holderOfA3Extent.object = a1; // Illegal
-//        holderOfA3Extent.object = a2; // Illegal
-//        holderOfA3Extent.object = a3; // Illegal
-//        holderOfA3Extent.object = a4; // Illegal
-//        holderOfA3Extent.object = a5; // Illegal
-
-        // Set exact type
-
-//        holderOfA3.set(o);  // Illegal
-//        holderOfA3.set(a1); // Illegal
-//        holderOfA3.set(a2); // Illegal
-        holderOfA3.set(a3);
-        holderOfA3.set(a4);
-        holderOfA3.set(a5);
-
-//        holderOfA3Super.set(o);  // Illegal
-//        holderOfA3Super.set(a1); // Illegal
-//        holderOfA3Super.set(a2); // Illegal
-        holderOfA3Super.set(a3);
-        holderOfA3Super.set(a4);
-        holderOfA3Super.set(a5);
-
-//        holderOfA3Extent.set(o);  // Illegal
-//        holderOfA3Extent.set(a1); // Illegal
-//        holderOfA3Extent.set(a2); // Illegal
-//        holderOfA3Extent.set(a3); // Illegal
-//        holderOfA3Extent.set(a4); // Illegal
-//        holderOfA3Extent.set(a5); // Illegal
-
-        // Set extent
-
-//        holderOfA3.setExtent(o);  // Illegal
-//        holderOfA3.setExtent(a1); // Illegal
-//        holderOfA3.setExtent(a2); // Illegal
-        holderOfA3.setExtent(a3);
-        holderOfA3.setExtent(a4);
-        holderOfA3.setExtent(a5);
-
-//        holderOfA3Super.setExtent(o);  // Illegal
-//        holderOfA3Super.setExtent(a1); // Illegal
-//        holderOfA3Super.setExtent(a2); // Illegal
-        holderOfA3Super.setExtent(a3);
-        holderOfA3Super.setExtent(a4);
-        holderOfA3Super.setExtent(a5);
-
-//        holderOfA3Extent.setExtent(o);  // Illegal
-//        holderOfA3Extent.setExtent(a1); // Illegal
-//        holderOfA3Extent.setExtent(a2); // Illegal
-//        holderOfA3Extent.setExtent(a3); // Illegal
-//        holderOfA3Extent.setExtent(a4); // Illegal
-//        holderOfA3Extent.setExtent(a5); // Illegal
-
-        // Set exact holder
-
-//        holderOfA3.set(holderOfObject); // Illegal
-//        holderOfA3.set(holderOfA1); // Illegal
-//        holderOfA3.set(holderOfA2); // Illegal
-        holderOfA3.set(holderOfA3);
-//        holderOfA3.set(holderOfA4); // Illegal
-//        holderOfA3.set(holderOfA5); // Illegal
-
-//        holderOfA3Super.set(holderOfObject); // Illegal
-//        holderOfA3Super.set(holderOfA1); // Illegal
-//        holderOfA3Super.set(holderOfA2); // Illegal
-//        holderOfA3Super.set(holderOfA3); // Illegal
-//        holderOfA3Super.set(holderOfA4); // Illegal
-//        holderOfA3Super.set(holderOfA5); // Illegal
-
-//        holderOfA3Extent.set(holderOfObject); // Illegal
-//        holderOfA3Extent.set(holderOfA1); // Illegal
-//        holderOfA3Extent.set(holderOfA2); // Illegal
-//        holderOfA3Extent.set(holderOfA3); // Illegal
-//        holderOfA3Extent.set(holderOfA4); // Illegal
-//        holderOfA3Extent.set(holderOfA5); // Illegal
-
-        // Set super holder
-
-        holderOfA3.setSuper(holderOfObject);
-        holderOfA3.setSuper(holderOfA1);
-        holderOfA3.setSuper(holderOfA2);
-        holderOfA3.setSuper(holderOfA3);
-//        holderOfA3.setSuper(holderOfA4); // Illegal
-//        holderOfA3.setSuper(holderOfA5); // Illegal
-
-        holderOfA3Super.setSuper(holderOfObject);
-//        holderOfA3Super.setSuper(holderOfA1); // Illegal
-//        holderOfA3Super.setSuper(holderOfA2); // Illegal
-//        holderOfA3Super.setSuper(holderOfA3); // Illegal
-//        holderOfA3Super.setSuper(holderOfA4); // Illegal
-//        holderOfA3Super.setSuper(holderOfA5); // Illegal
-
-        holderOfA3Extent.setSuper(holderOfObject);
-        holderOfA3Extent.setSuper(holderOfA1);
-        holderOfA3Extent.setSuper(holderOfA2);
-        holderOfA3Extent.setSuper(holderOfA3);
-//        holderOfA3Extent.setSuper(holderOfA4); // Illegal
-//        holderOfA3Extent.setSuper(holderOfA5); // Illegal
-
-        // Set extent holder
-
-//        holderOfA3.setExtent(holderOfObject); // Illegal
-//        holderOfA3.setExtent(holderOfA1); // Illegal
-//        holderOfA3.setExtent(holderOfA2); // Illegal
-        holderOfA3.setExtent(holderOfA3);
-        holderOfA3.setExtent(holderOfA4);
-        holderOfA3.setExtent(holderOfA5);
-
-//        holderOfA3Super.setExtent(holderOfObject); // Illegal
-//        holderOfA3Super.setExtent(holderOfA1); // Illegal
-//        holderOfA3Super.setExtent(holderOfA2); // Illegal
-        holderOfA3Super.setExtent(holderOfA3);
-        holderOfA3Super.setExtent(holderOfA4);
-        holderOfA3Super.setExtent(holderOfA5);
-
-//        holderOfA3Extent.setExtent(holderOfObject); // Illegal
-//        holderOfA3Extent.setExtent(holderOfA1); // Illegal
-//        holderOfA3Extent.setExtent(holderOfA2); // Illegal
-//        holderOfA3Extent.setExtent(holderOfA3); // Illegal
-//        holderOfA3Extent.setExtent(holderOfA4); // Illegal
-//        holderOfA3Extent.setExtent(holderOfA5); // Illegal
-
-
-
-        // Invariance, Covariance and Contravariance
 
         Holder<A2> holderA2 = new Holder<>();
         Holder<A3> holderA3 = new Holder<>();
@@ -187,6 +27,7 @@ public class LearnGenerics {
         Holder<? super A2> superHolder2 = new Holder<>();
         Holder<? super A3> superHolder3 = new Holder<>();
         Holder<? super A4> superHolder4 = new Holder<>();
+
 
         // Set Concrete Holder
 
@@ -202,6 +43,7 @@ public class LearnGenerics {
 //        holderA3 = superHolder3; // Illegal
 //        holderA3 = superHolder4; // Illegal
 
+
         // Set Extent Holder
 
 //        extentHolder3 = holderA2; // Illegal
@@ -216,6 +58,7 @@ public class LearnGenerics {
 //        extentHolder3 = superHolder3; // Illegal
 //        extentHolder3 = superHolder4; // Illegal
 
+
         // Set Super Holder
 
         superHolder3 = holderA2;
@@ -229,6 +72,7 @@ public class LearnGenerics {
         superHolder3 = superHolder2;
         superHolder3 = superHolder3;
 //        superHolder3 = superHolder4; // Illegal
+
 
         // Set/Get value of Concrete Holder
 
@@ -250,6 +94,7 @@ public class LearnGenerics {
         holderA3.object.a3();
 //        holderA3.object.a4(); // Illegal
 //        holderA3.object.a5(); // Illegal
+
 
         // Set/Get value of Extent Holder
 
@@ -273,6 +118,7 @@ public class LearnGenerics {
         extentHolder3.object.a3();
 //        extentHolder3.object.a4(); // Illegal
 //        extentHolder3.object.a5(); // Illegal
+
 
         // Set/Get value of Super Holder
 
@@ -302,33 +148,7 @@ public class LearnGenerics {
 
 
 class Holder <T> {
-
     public T object;
-
-    public T get() {
-        return object;
-    }
-
-    public void set(T object) {
-        this.object = object;
-    }
-
-    public <E extends T> void setExtent(E object) {
-        this.object = object;
-    }
-
-    public void set(Holder<T> holder) {
-        this.object = holder.get();
-    }
-
-    public void setExtent(Holder<? extends T> holder) {
-        this.object = holder.get();
-    }
-
-    public void setSuper(Holder<? super T> holder) {
-//        this.object = holder.get(); // Illegal
-    }
-
 }
 
 
