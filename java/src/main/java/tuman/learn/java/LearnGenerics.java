7d5214ba7074c8c883ae2cac549632b1760add44
Sergei Tumanov <tumanser@gmail.com>
2022-05-08 11:40:11 +0300

    Refactoring

--- java/src/main/java/tuman/learn/java/LearnGenerics.java
+++ java/src/main/java/tuman/learn/java/LearnGenerics.java
@@ -1,6 +1,9 @@
 package tuman.learn.java;
 
 
+import tuman.learn.java.utils.ObjectHolder;
+
+
 public class LearnGenerics {
 
     public static void main(String[] args) {
@@ -10,29 +13,22 @@ public class LearnGenerics {
     }
 
     public void superAndExtends() {
-        Holder<AA, ? super AA, ? extends AA> holder = new Holder<>();
+        ObjectHolder<AA> holderOfAa = new ObjectHolder<>();
+        ObjectHolder<? super AA> holderOfAaSuper = new ObjectHolder<>();
+        ObjectHolder<? extends AA> holderOfAaExtent = new ObjectHolder<>();
 
-//        holder.concreteHolder = new A(); // OK, Illegal
-        holder.concreteHolder = new AA();  // OK
-        holder.concreteHolder = new AAA(); // OK
+//        holderOfAa.set(new A()); // Illegal
+        holderOfAa.set(new AA());
+        holderOfAa.set(new AAA());
 
-//        holder.superHolder = new A(); // TODO: Why illegal
-        holder.superHolder = new AA();  // OK
-        holder.superHolder = new AAA(); // TODO: Why illegal
+//        holderOfAaSuper.set(new A()); // Illegal
+        holderOfAaSuper.set(new AA());
+        holderOfAaSuper.set(new AAA());
 
-//        holder.extendsHolder = new A();   // TODO: Why illegal
-//        holder.extendsHolder = new AA();  // TODO: Why illegal
-//        holder.extendsHolder = new AAA(); // TODO: Why illegal
+//        holderOfAaExtent.set(new A());   // Illegal
+//        holderOfAaExtent.set(new AA());  // Illegal
+//        holderOfAaExtent.set(new AAA()); // Illegal
     }
-
-}
-
-
-class Holder <C, S, E> {
-
-    C concreteHolder;
-    S superHolder;
-    E extendsHolder;
 
 }
 
@@ -52,23 +48,5 @@ class AA extends A {
 class AAA extends AA {
     public void aaa() {
         System.out.println("A-A-A");
-    }
-}
-
-class AAB extends AA {
-    public void aab() {
-        System.out.println("A-A-B");
-    }
-}
-
-class AB extends A {
-    public void aa() {
-        System.out.println("A-B");
-    }
-}
-
-class B {
-    public void aa() {
-        System.out.println("B");
     }
 }
