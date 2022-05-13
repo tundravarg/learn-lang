8fe6c552cbb73020ebb95bbf2bcdd9a01233279c
Sergei Tumanov <tumanser@gmail.com>
2022-05-13 09:36:47 +0300

    MultiRunThread

--- java/src/main/java/tuman/learn/java/LearnThreads.java
+++ java/src/main/java/tuman/learn/java/LearnThreads.java
@@ -7,6 +7,9 @@ import tuman.learn.java.utils.TestRun;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.BiFunction;
 import java.util.stream.IntStream;
 
 
@@ -17,7 +20,8 @@ public class LearnThreads {
         LearnThreads learnThreads = new LearnThreads();
 
 //        learnThreads.basicThreads();
-        learnThreads.restartThread();
+//        learnThreads.restartThread();
+        learnThreads.reuseThread();
     }
 
 
@@ -58,7 +62,7 @@ public class LearnThreads {
     }
 
     private void restartThread() {
-        TestRun.run("Reuse Thread", (name, out) -> {
+        TestRun.run("Restart Thread", (name, out) -> {
             final var thread = new Thread(() -> {
                 out.out("Thread run");
             });
@@ -75,6 +79,157 @@ public class LearnThreads {
         });
     }
 
+    private void reuseThread() {
+        TestRun.run("Reuse Thread", (name, out) -> {
+
+            final var thread = new MultiRunThread();
+            thread.start();
+
+            final BiFunction<String, Double, Runnable> task = (taskName, duration) -> () -> {
+                out.out("Begin %s", taskName);
+                MiscUtils.sleep(duration);
+                out.out("End %s", taskName);
+            };
+
+            thread.addTask(task.apply("1", 1.0));
+            thread.addTask(task.apply("2", 1.0));
+            thread.addTask(task.apply("3", 1.0));
+            thread.addTask(task.apply("4", 1.0));
+            thread.addTask(task.apply("5", 1.0));
+
+            MiscUtils.sleep(2.5);
+
+            thread.finishNow(); // TODO
+//            thread.finishAfterCurrentTask();
+//            thread.finishAfterAllTasks();
+            out.out("Finish requested");
+
+            thread.addTask(task.apply("6", 1.0));
+            thread.addTask(task.apply("7", 1.0));
+
+            MiscUtils.join(thread);
+
+        });
+    }
+
 }
 
-class MyThread extends Thread {}
+
+class MultiRunThread extends Thread {
+
+    private enum State {
+        READY,
+        RUN,
+        FINISH_NOW,
+        FINISH_AFTER_CURRENT_TASK,
+        FINISH_AFTER_ALL_TASKS,
+        FINISHED
+    }
+
+    private static final long PAUSE_IF_NO_TASKS = 1000;
+
+    private Queue<Runnable> tasks = new ConcurrentLinkedQueue<>();
+    private State state = State.READY;
+
+
+    @Override
+    public void run() {
+        state = State.RUN;
+        loop: while (true) {
+            if (isInterrupted()) {
+                break loop;
+            }
+            switch (state) {
+                case READY -> {
+                    throw new IllegalStateException("Thread isn't started");
+                }
+                case FINISHED, FINISH_NOW, FINISH_AFTER_CURRENT_TASK -> {
+                    break loop;
+                }
+                case FINISH_AFTER_ALL_TASKS -> {
+                    if (tasks.isEmpty()) {
+                        break loop;
+                    }
+                }
+            }
+
+            Runnable task = tasks.poll();
+            if (task == null) {
+                MiscUtils.sleep(PAUSE_IF_NO_TASKS);
+                continue loop;
+            }
+
+            try {
+                task.run();
+            } catch (Exception ex) {
+                System.err.printf("Task failed: %s: %s\n", ex.getClass().getSimpleName(), ex.getMessage());
+            }
+        }
+        state = State.FINISHED;
+    }
+
+
+    public void addTask(Runnable task) {
+        if (isReadyForNewTasks()) {
+            this.tasks.add(task);
+        } else {
+            throw new IllegalStateException("Illegal state: " + state);
+        }
+    }
+
+
+    public void finishNow() {
+        if (isInRunState()) {
+            this.interrupt();
+            state = State.FINISH_NOW;
+        } else {
+            throw new IllegalStateException("Illegal state: " + state);
+        }
+    }
+
+    public void finishAfterCurrentTask() {
+        if (isInRunState()) {
+            state = State.FINISH_AFTER_CURRENT_TASK;
+        } else {
+            throw new IllegalStateException("Illegal state: " + state);
+        }
+    }
+
+    public void finishAfterAllTasks() {
+        if (isInRunState()) {
+            state = State.FINISH_AFTER_ALL_TASKS;
+        } else {
+            throw new IllegalStateException("Illegal state: " + state);
+        }
+    }
+
+
+    private boolean isInRunState() {
+        if (isInterrupted()) {
+            return false;
+        }
+        switch (state) {
+            case READY, RUN, FINISH_NOW, FINISH_AFTER_CURRENT_TASK, FINISH_AFTER_ALL_TASKS -> {
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    private boolean isReadyForNewTasks() {
+        if (isInterrupted()) {
+            return false;
+        }
+        switch (state) {
+            case READY, RUN, FINISH_AFTER_ALL_TASKS -> {
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+}
