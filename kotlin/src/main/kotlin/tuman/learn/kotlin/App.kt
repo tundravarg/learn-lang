006fb0065d0cd1e062e939ee474d4fe53d7bf10d
Sergey Tumanov <tumanser@gmail.com>
2022-06-14 09:04:17 +0300

    Access modiers in file

--- kotlin/src/main/kotlin/tuman/learn/kotlin/App.kt
+++ kotlin/src/main/kotlin/tuman/learn/kotlin/App.kt
@@ -1,7 +1,21 @@
 package tuman.learn.kotlin
 
 
-// fun main(args: Array<String>) {
+import tuman.learn.kotlin.subpackage.defaultFun
+import tuman.learn.kotlin.subpackage.publicFun
+// import tuman.learn.kotlin.subpackage.privateFun    // ILLEGAL: `private` access
+
+
 fun main(args: Array<String>) {
-    System.out.println("Learn Kotlin " + (if (args.size > 0) args[0] else ""));
+    System.out.println("Learn Kotlin " + (if (args.size > 0) args[0] else ""))
+    learnAccessModifiers()
+}
+
+
+private fun learnAccessModifiers() {
+    println("-- accessModifiers --")
+
+    defaultFun()
+    publicFun()
+    // privateFun()    // ILLEGAL: `private` access
 }
